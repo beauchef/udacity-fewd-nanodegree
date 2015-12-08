33c1a8c3677ed8eba27929f612b251d74876e20d
Jean-François Beauchef <jean-francois@beauchef.com>
2015-12-07 22:07:54 -0500

    Make player move

--- P3_arcade_game/js/app.js
+++ P3_arcade_game/js/app.js
@@ -1,39 +1,134 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+/**
+ *
+ * Jean-François Beauchef
+ *
+ * Udacity P3: Classic Arcade Game Clone
+ *
+ * TODO:
+ * - make enemies move
+ *
+ */
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+
+var TILE_FULL_HEIGHT = 171; // Height of a tile, including the hidden portion.
+var TILE_HEIGHT = 83;
+var TILE_WIDTH = 101;
+var TILE_HEIGHT_OFFSET_ENEMY = 25;
+var TILE_HEIGHT_OFFSET_PLAYER = 10;
+
+
+
+/**
+ * Character class
+ *
+ * @param imageUrl
+ * @constructor
+ */
+var Character = function(imageUrl, tileX, tileY, heightOffset) {
+    this.sprite = imageUrl;
+    this.heightOffset = heightOffset;
+    this.x = (tileX - 1) * TILE_WIDTH;
+    this.y = ((tileY - 1) * TILE_HEIGHT) - heightOffset;
+}
+
+/**
+ * Update the character's position, required method for game
+ *
+ * @param dt, a time delta between ticks
+ */
+Character.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+/**
+ * Draw the character on the screen, required method for game
+ */
+Character.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+/**
+ * Enemies our player must avoid
+ *
+ * @constructor
+ */
+var Enemy = function(tileX, tileY) {
+    Character.call(this, 'images/enemy-bug.png', tileX, tileY, TILE_HEIGHT_OFFSET_ENEMY);
+};
+
+Enemy.prototype = Object.create(Character.prototype);
+Enemy.prototype.constructor = Enemy;
 
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+/**
+ * Player class, which subclasses the Character class
+ *
+ * @constructor
+ */
+var Player = function(tileX, tileY) {
+    Character.call(this, 'images/char-boy.png', tileX, tileY, TILE_HEIGHT_OFFSET_PLAYER);
+};
+
+Player.prototype = Object.create(Character.prototype);
+Player.prototype.constructor = Player;
+
+/**
+ * Allows the player to move when an arrow key is pressed.
+ * This is specific to the player.
+ *
+ * @param input
+ */
+Player.prototype.handleInput = function(input) {
+    if (input !== undefined) {
+        var newX = this.x;
+        var newY = this.y;
+        switch (input) {
+            case 'left':
+                newX = this.x - TILE_WIDTH;
+                break;
+            case 'right':
+                newX = this.x + TILE_WIDTH;
+                break;
+            case 'up':
+                newY = this.y - TILE_HEIGHT;
+                break;
+            case 'down':
+                newY = this.y + TILE_HEIGHT;
+                break;
+            default:
+                console.error('Invalid input: ' + input)
+                break;
+        }
+        console.log('newX='+newX+', newY='+newY);
+        this.x = (newX >= 0 && newX <= ctx.canvas.width - TILE_WIDTH) ? newX : this.x;
+        this.y = (newY >= -this.heightOffset && newY <= ctx.canvas.height - TILE_FULL_HEIGHT - this.heightOffset) ? newY : this.y;
+    }
+};
+
+
+
+/**
+ * Now instantiate your objects.
+ * Place all enemy objects in an array called allEnemies
+ * Place the player object in a variable called player
+ *
+ */
+var allEnemies = [new Enemy(1, 2), new Enemy(5, 3), new Enemy(2, 4)];
+var player = new Player(3, 6);
+
+
+
+/**
+ * This listens for key presses and sends the keys to your
+ * Player.handleInput() method. You don't need to modify this.
+ *
+ */
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -41,6 +136,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
