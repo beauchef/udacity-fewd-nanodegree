e11253e6e313d71ff105eb030f64994a42d083d2
Jean-François Beauchef <jean-francois@beauchef.com>
2015-12-08 14:18:43 -0500

    Make the enemies move

--- P3_arcade_game/js/app.js
+++ P3_arcade_game/js/app.js
@@ -5,8 +5,10 @@
  * Udacity P3: Classic Arcade Game Clone
  *
  * TODO:
- * - make enemies move
- *
+ * - make enemies move to the other side of the screen without disappearing
+ * - collision detection
+ * - reset player position once the top is reached
+ * 
  */
 
 
@@ -17,18 +19,29 @@ var TILE_WIDTH = 101;
 var TILE_HEIGHT_OFFSET_ENEMY = 25;
 var TILE_HEIGHT_OFFSET_PLAYER = 10;
 
-var MOVE = {
-    'left': { 'x': -TILE_WIDTH, 'y': 0 },
-    'right': { 'x': +TILE_WIDTH, 'y': 0 },
-    'up': { 'x': 0, 'y': -TILE_HEIGHT },
-    'down': { 'x': 0, 'y': +TILE_HEIGHT }
+var ENEMY_SPEED = {
+    'slow': 100,
+    'medium': 150,
+    'fast': 200
+};
+
+var PLAYER_INITIAL_TILE = { 'x': 3, 'y': 6 };
+
+var PLAYER_MOVE = {
+    'left':  { 'x': -TILE_WIDTH,  'y': 0 },
+    'right': { 'x': +TILE_WIDTH,  'y': 0 },
+    'up':    { 'x': 0,            'y': -TILE_HEIGHT },
+    'down':  { 'x': 0,            'y': +TILE_HEIGHT }
 };
 
 
 /**
  * Character class
  *
- * @param imageUrl
+ * @param imageUrl URL to the image representing this character
+ * @param tileX tile position on X axis
+ * @param tileY tile position on Y axis
+ * @param heightOffset height offset in pixels for this character
  * @constructor
  */
 var Character = function(imageUrl, tileX, tileY, heightOffset) {
@@ -40,13 +53,14 @@ var Character = function(imageUrl, tileX, tileY, heightOffset) {
 
 /**
  * Update the character's position, required method for game
+ * You should multiply any movement by the dt parameter
+ * which will ensure the game runs at the same speed for
+ * all computers.
  *
  * @param dt, a time delta between ticks
  */
 Character.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // do nothing
 };
 
 /**
@@ -57,27 +71,48 @@ Character.prototype.render = function() {
 };
 
 
-
 /**
  * Enemies our player must avoid
  *
+ * @param tileX tile position on X axis
+ * @param tileY tile position on Y axis
  * @constructor
  */
-var Enemy = function(tileX, tileY) {
+var Enemy = function(tileX, tileY, speed) {
+    this.speed = speed;
     Character.call(this, 'images/enemy-bug.png', tileX, tileY, TILE_HEIGHT_OFFSET_ENEMY);
 };
 
 Enemy.prototype = Object.create(Character.prototype);
 Enemy.prototype.constructor = Enemy;
 
+/**
+ * Update the enemy's position.
+ * Dependant on the enemy's specific speed.
+ *
+ * @param dt, a time delta between ticks
+ */
+Enemy.prototype.update = function(dt) {
+    if (dt !== undefined) {
+        this.x = this.x + (this.speed * dt);
+        if (this.x > (ctx.canvas.width - TILE_WIDTH)) {
+            this.x = this.x - (ctx.canvas.width - TILE_WIDTH);
+        }
+    }
+};
+
 
 
 /**
  * Player class, which subclasses the Character class
  *
+ * @param tileX tile position on X axis
+ * @param tileY tile position on Y axis
  * @constructor
  */
 var Player = function(tileX, tileY) {
+    tileX = typeof tileX !== 'undefined' ? tileX : PLAYER_INITIAL_TILE.x;
+    tileY = typeof tileY !== 'undefined' ? tileY : PLAYER_INITIAL_TILE.y;
     Character.call(this, 'images/char-boy.png', tileX, tileY, TILE_HEIGHT_OFFSET_PLAYER);
 };
 
@@ -88,13 +123,13 @@ Player.prototype.constructor = Player;
  * Allows the player to move when an arrow key is pressed.
  * This is specific to the player.
  *
- * @param input
+ * @param input key input (can be: 'left', 'right', 'up', 'down')
  */
 Player.prototype.handleInput = function(input) {
-    // First check that input is not undefined, and that it is a valid input in the MOVE matrix
-    if (input !== undefined && Object.prototype.hasOwnProperty.call(MOVE, input)) {
-        var newX = this.x + MOVE[input]['x'];
-        var newY = this.y + MOVE[input]['y'];
+    // First check that input is not undefined, and that it is a valid input in the PLAYER_MOVE matrix
+    if (input !== undefined && Object.prototype.hasOwnProperty.call(PLAYER_MOVE, input)) {
+        var newX = this.x + PLAYER_MOVE[input]['x'];
+        var newY = this.y + PLAYER_MOVE[input]['y'];
         this.x = (newX >= 0 && newX <= ctx.canvas.width - TILE_WIDTH) ? newX : this.x;
         this.y = (newY >= -this.heightOffset && newY <= ctx.canvas.height - TILE_FULL_HEIGHT - this.heightOffset) ? newY : this.y;
     }
@@ -106,10 +141,16 @@ Player.prototype.handleInput = function(input) {
  * Now instantiate your objects.
  * Place all enemy objects in an array called allEnemies
  * Place the player object in a variable called player
+ * Positions here are based on tiles, not pixels.
+ * The top left tile is (1,1). The bottom right rile is (5,6).
  *
  */
-var allEnemies = [new Enemy(1, 2), new Enemy(5, 3), new Enemy(2, 4)];
-var player = new Player(3, 6);
+var allEnemies = [
+    new Enemy(1, 2, ENEMY_SPEED.medium),
+    new Enemy(4, 3, ENEMY_SPEED.fast),
+    new Enemy(2, 4, ENEMY_SPEED.slow)
+];
+var player = new Player();
 
 
 
