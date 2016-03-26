fcef96526c8500a8a24c976fba598dcd84644083
Jean-François Beauchef <jean-francois@beauchef.com>
2016-03-26 18:20:28 -0400

    Added Wikipedia integration

--- P5_1_neighborhood_map/js/script.js
+++ P5_1_neighborhood_map/js/script.js
@@ -1,4 +1,3 @@
-
 function loadData() {
 
     var $body = $('body');
@@ -11,8 +10,8 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    var street = $( "input#street" ).val();
-    var city = $( "input#city" ).val();
+    var street = $("input#street").val();
+    var city = $("input#city").val();
     var address = street + ", " + city;
     $greeting.text('So, you want to live at ' + address + '?');
 
@@ -35,14 +34,50 @@ function loadData() {
         $nytHeaderElem.text('NY Times articles about ' + city);
         console.log(data.response);
         var items = [];
-        $.each(data.response.docs, function( key, val ) {
+        $.each(data.response.docs, function(key, val) {
             var article = '<a href="' + val.web_url + '">' + val.headline.main + '</a><p>' + val.snippet + '</p>';
-            items.push( "<li id='nytimes-article-" + key + "'>" + article + "</li>" );
+            items.push("<li id='nytimes-article-" + key + "'>" + article + "</li>");
         });
         $("ul#nytimes-articles").html(items.join(""));
     }).fail(function(error) {
         var errorMsg = "NY Times articles could not be loaded";
         $nytHeaderElem.text(errorMsg);
+        console.log(errorMsg);
+    });
+
+    // wikipedia
+    var wikiSearch = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='
+            + city
+            + '&format=json&callback=wikiCallback';
+    var wikiTimeout = setTimeout(function() {
+        $wikiElem.text("Failed to get Wikipedia resources");
+    }, 8000)
+    $.ajax({
+        url: wikiSearch,
+        type: 'GET',
+        dataType: 'jsonp',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Api-User-Agent': 'Udacity-JF-Beauchef/1.0 (http://beauchef.github.io/; jean-francois_at_beauchef.com)'
+        }
+    }).done(function(data) {
+        clearTimeout(wikiTimeout);
+        $wikiElem.text('Wikipedia articles about ' + city);
+        console.log(data);
+        var items = [];
+        // element 0 is search terms
+        var titles = data[1];
+        var texts = data[2];
+        var urls = data[3];
+        $.each(titles, function(i, title) {
+            var articleLink = '<a href="' + urls[i] + '">' + title + '</a>';
+            var articleContent = '<p>' + texts[i] + '</p>';
+            items.push("<li id='wikipedia-article-" + i + "'>" + articleLink + articleContent + "</li>");
+        });
+        $("ul#wikipedia-links").html(items.join(""));
+    }).fail(function(error) {
+        var errorMsg = "Wikipedia articles could not be loaded";
+        $wikiElem.text(errorMsg);
         console.log(errorMsg);
     });
 
